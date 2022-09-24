7d1f647f800e9094ad6927e51e864d4a91c5fcee
SaraiFerrer <87380666+SaraiFerrer@users.noreply.github.com>
2022-09-23 20:35:06 -0500

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
